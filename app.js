e3923018e52f4e2f468eeac19e1dff2742b1232b
mansipadgelwar <mpadgelwar@gmail.com>
2021-07-27 10:06:06 +0530

    feat: display no of notes in table

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var cashInput = document.querySelector("#cash-input");
 var btnSubmit = document.querySelector("#btn-submit");
 var btnCheck = document.querySelector("#btn-check");
 var cashDiv = document.querySelector("#cashGiven");
+var notesToReturn = document.querySelectorAll(".tableData");
 var table = document.querySelector("#changeTable");
 var error = document.querySelector("#error");
+
+let noOfNotes = [];
+let notesAvailable = [2000,500,100,20,10,5,1];
 
 
 btnSubmit.addEventListener("click",()=>{
@@ -32,6 +36,29 @@ btnCheck.addEventListener("click", ()=>{
         
     }
     
+
+
+ let returnAmount = parseInt(cashInput) - parseInt(billAmount);
+ //console.log(returnAmount);
+ 
+ for(let i = 0;i < notesAvailable.length;i++){
+     noOfNotes[i] = Math.floor(returnAmount/notesAvailable[i]);
+     returnAmount = returnAmount - notesAvailable[i] * noOfNotes[i];
+     notesToReturn[i].innerHTML = noOfNotes[i];
+     //console.log(noOfNotes[i]);
+    if(returnAmount === 0){
+        break;
+    }
+ }
+
+ for(const notesAvailable of notesToReturn){
+     if(notesAvailable.innerHTML == "0"){
+         notesAvailable.innerHTML = "";
+     }
+ }
+
+ noOfNotes = [];
+
 });
 
-    
+
